d96fe629484bcef782dadcda7bc1bae8c8035e4a
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 08:56:13 +0100

    in theory finished shell scripts

--- shellScripts/pdfLiveUpdate.sh
+++ shellScripts/pdfLiveUpdate.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+basename="$1"
+
+if pdflatex -halt-on-error -file-line-error -synctex=1 ${basename} ; then
+    if grep -i "undefined citations\|undefined references" ${basename}.log ; then
+	if biber ${basename}.tex ; then
+	    pdflatex -halt-on-error -file-line-error -synctex=1 ${basename}
+	fi
+    fi
+    if grep -i -q "rerun to get" ${basename}.log ; then
+	pdflatex -halt-on-error -file-line-error -synctex=1 ${basename}
+    fi
+fi
+
 
